a64a9a0228f620a57b65a1090fcb2dde407216ee
Salma Magdy Ishak <salma.m.ishakk@gmail.com>
2022-11-28 00:13:12 +0200

    Fixed english OCR error

--- EnglishOCR.py
+++ EnglishOCR.py
@@ -5,6 +5,6 @@ from PIL import Image
 pytesseract.pytesseract.tesseract_cmd = r"F:\Users\salma\AppData\Local\Programs\OCR tesseract\tesseract.exe"
 def convertToEngText(path):
     img = Image.open(path)
-    text = pytesseract.image_to_string(img, lang="en")
+    text = pytesseract.image_to_string(img, lang="eng")
     return text
 
