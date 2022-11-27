e4d312ae256b38d7357978b9a1f6f9456f3fcb0d
Salma Magdy Ishak <salma.m.ishakk@gmail.com>
2022-11-27 03:26:12 +0200

    Edit Arabic OCR

--- ArabicOCR.py
+++ ArabicOCR.py
@@ -3,7 +3,7 @@ import os
 from PIL import Image
 
 pytesseract.pytesseract.tesseract_cmd = r"F:\Users\salma\AppData\Local\Programs\OCR tesseract\tesseract.exe"
-def convertToEngText(path):
+def convertToArabicText(path):
     img = Image.open(path)
     text = pytesseract.image_to_string(img, lang="ara")
     return text
